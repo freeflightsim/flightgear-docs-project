d26a772f34637d4423dbb322ee48d95932688341
Peter Morgan <pete@daffodil.uk.com>
2012-10-01 21:30:51 +0100

    git

---
+++ py_update.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import yaml
+import git
+
+ROOT = os.path.abspath( os.path.dirname(__file__) )
+
+print "ROOT=%s" % ROOT
+
+## Load Config
+yaml_file = open("config.yaml")
+conf = yaml.load( yaml_file.read() )
+yaml_file.close()
+print "Loaded config: %s" % " ".join( conf.keys() )
+
+
+
+
+
